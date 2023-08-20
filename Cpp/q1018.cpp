1397d53f46c6d095bdb2b3dfff4d01d23951d34c
Raykkoner Dujhkkovick Silva de Farias <raykkonerd@gmail.com>
2023-08-19 21:26:43 -0300

    Update q1018.cpp - Versão mais eficaz

--- Cpp/q1018.cpp
+++ Cpp/q1018.cpp
@@ -4,37 +4,16 @@ using namespace std;
  
 int main() {
  
-    int num, a, b, c, d, e, f;
+    int num, n;
     int notas[7] = {100, 50, 20, 10, 5, 2, 1};
     cin >> num;
     
     cout << num << endl;
-    
-    a = (num/100);
-    cout << a << " nota(s) de R$ 100,00" << endl;
-    num -= (a*100);
-    
-    b = (num/50);
-    cout << b << " nota(s) de R$ 50,00" << endl;
-    num -= (b*50);
-    
-    c = (num/20);
-    cout << c << " nota(s) de R$ 20,00" << endl;
-    num -= (c*20);
-    
-    d = (num/10);
-    cout << d << " nota(s) de R$ 10,00" << endl;
-    num -= (d*10);
-    
-    e = (num/5);
-    cout << e << " nota(s) de R$ 5,00" << endl;
-    num -= (e*5);
-    
-    f = (num/2);
-    cout << f << " nota(s) de R$ 2,00" << endl;
-    num -= (f*2);
-    
-    cout << num << " nota(s) de R$ 1,00" << endl;
+    for(int nota : notas){
+        n = (num/nota);
+        cout << n << " nota(s) de R$ " << nota << ",00" << endl;
+        num -= (n*nota);
+    }
  
     return 0;
 }
